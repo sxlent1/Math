a02a0d68096a3bfdf1496ebc997590e7d04065ba
sxlent1 <152503534+sxlent1@users.noreply.github.com>
2023-11-30 16:48:30 +0100

    Math
    .

--- main.py
+++ main.py
@@ -1,8 +1,158 @@
-print  ("press ctrl+alt+t")
 import pyautogui as pg
 import time
-time.sleep (5)
-while True:
-    pg.write ('pizza')
-    pg.press ('enter')
-    pg.write ('nemtom')
+Tomi = "Tomi"
+name = Tomi
+
+print("Hello" + " ")
+print(f' ')
+print(f'Jelenleg Python-t tanulok!')
+time.sleep(1)
+print(f'{name} vagyok és írtam egy programot, ami készít neked egy Matematika feladat sort.')
+time.sleep(6)
+
+A = "-8+"
+B = "(+5)="
+C = -3
+E = "-10+(-6)"
+D = "-2-(-6)"
+F = "-7+(+7)"
+G = "2+(-3)"
+H = "-6+(-2)"
+I = "-2+(-8)"
+J = "-5+(2)"
+K = "-1-(-1)"
+L = ("+40+(-20)")
+
+print('')
+for i in range(10, 0, -1):
+    time.sleep(1)
+    print(f' {A}{B}')
+    print(i)
+    print('')
+
+print(f'A válasz {C}')
+
+print(f'Na jó lett? '
+      f'01/10')
+
+print (f' Várj!')
+time.sleep(3)
+
+for i in range(10, 0, -1):
+    time.sleep(1)
+    print(f' {D}')
+    print(i)
+    print('')
+
+eredmeny1 = int(input("Add meg a válaszodat: "))
+print(f'A helyes válasz: 4 '
+      f'02/10')
+
+print (f' Várj!')
+time.sleep(3)
+
+for i in range(10, 0, -1):
+    time.sleep(1)
+    print(f' {E}')
+    print(i)
+    print('')
+
+eredmeny2 = int(input("Add meg a válaszodat: "))
+print(f'A helyes válasz: -16'
+      f' '
+      f'03/10')
+
+print (f' Várj!')
+time.sleep(3)
+
+for i in range (10, 0, -1):
+    time.sleep(1)
+    print(f' {F}')
+    print(i)
+    print('')
+
+eredmeny3 = int(input("Add meg a válaszodat: "))
+print(f'A helyes válasz: 0  04/10')
+
+print (f' Várj!')
+time.sleep (3)
+
+for i in range (10, 0, -1):
+    time.sleep(1)
+    print(f' {G}')
+    print(i)
+    print('')
+
+eredmeny4 = int(input("Add meg a válaszodat: "))
+print(f'A helyes válasz: -1 '
+      f'05/10')
+
+print (f' Várj!')
+time.sleep(3)
+
+for i in range (10, 0, -1):
+    time.sleep(1)
+    print(f' {H}')
+    print(i)
+    print('')
+
+eredmeny4 = int(input("Add meg a válaszodat: "))
+print(f'A helyes válasz: -8 '
+      f'06/10')
+
+print (f' Várj!')
+time.sleep(3)
+
+for i in range (10, 0, -1):
+    time.sleep(1)
+    print(f' {I}')
+    print(i)
+    print('')
+
+eredmeny4 = int(input("Add meg a válaszodat: "))
+print(f'A helyes válasz: -10 '
+      f'07/10')
+
+print (f' Várj!')
+time.sleep(3)
+
+for i in range (10, 0, -1):
+    time.sleep(1)
+    print(f' {J}')
+    print(i)
+    print('')
+
+eredmeny4 = int(input("Add meg a válaszodat: "))
+print(f'A helyes válasz: -3 '
+      f'08/10')
+
+print (f' Várj!')
+time.sleep(3)
+
+for i in range (10, 0, -1):
+    time.sleep(1)
+    print(f' {K}')
+    print(i)
+    print('')
+
+eredmeny4 = int(input("Add meg a válaszodat: "))
+print(f'A helyes érték: 0 '
+      f'09/10')
+
+print (f' Várj!')
+time.sleep(3)
+
+for i in range (10, 0, -1):
+    time.sleep(1)
+    print(f' {L}')
+    print(i)
+    print('')
+
+eredmeny5 = int(input("Add meg a válaszodat: "))
+print(f'A helyes érték: 20'
+      f' 10/10')
+
+print (f' Várj!')
+time.sleep (3)
+
+print (f'Gratulálok!')
